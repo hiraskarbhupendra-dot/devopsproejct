9c55ff127897d84bc8f8273075547af9b529b9a7
hiraskarbhupendra-dot <hiraskar.bhupendra@gmail.com>
2026-07-04 13:19:23 +0000

    add one more name

--- name.java
+++ name.java
@@ -3,3 +3,9 @@ public class Main {
         System.out.println("John");
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bhupendra");
+    }
+}
+
